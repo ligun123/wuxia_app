43822496fa3dce37898acff3805d4d19f2836b91
ligun <ligun@cechealth.cn>
2020-05-13 17:48:06 +0800

    fix: 移除多余代码

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       initialRoute: 'XBottomController',
